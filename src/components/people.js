2f8fa358c5ce830aca1b8cbe19c3fff27a077aa3
jbeltrami <joao_beltrami@hotmail.com>
2019-05-02 10:42:34 -0400

    Add age filter to people

--- src/components/people.js
+++ src/components/people.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import Person from './person';
+import PeopleAgeFilter from './people-filter';
 
 class People extends Component {
   state = {
     people: [],
+    ages: [20, 40, 60, 70],
   };
 
   componentDidMount() {
@@ -18,11 +20,20 @@ class People extends Component {
 
         return apiResponse.sort((a, b) => a.name.last - b.name.last);
       })
-      .then(res =>
+      .then(res => {
         this.setState(() => ({
           people: [...res],
-        }))
-      );
+        }));
+      });
+  };
+
+  handleAgeFilter = age => {
+    const { people } = this.state;
+    const filteredPeople = people.filter(e => e.dob.age > age);
+
+    this.setState({
+      people: [...filteredPeople],
+    });
   };
 
   handleDelete = id => {
@@ -35,7 +46,7 @@ class People extends Component {
   };
 
   render() {
-    const { people } = this.state;
+    const { people, ages } = this.state;
 
     return (
       <div>
@@ -46,6 +57,13 @@ class People extends Component {
               <h3>based on: https://randomuser.me/documentation </h3>
             </div>
           </div>
+
+          <div className="row">
+            <div className="col-lg-12">People Age Filter:</div>
+          </div>
+
+          <PeopleAgeFilter ages={ages} onFilter={this.handleAgeFilter} />
+
           <div className="row align-item-center justify-content-center">
             {people.map((person, index) => (
               <Person {...person} key={index} onDelete={this.handleDelete} />
--- src/components/person.js
+++ src/components/person.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Person(props) {
-  const { name, dob, email, onDelete, id } = props;
+  const { name, dob, email } = props;
   return (
     <div className="col-lg-3 my-4">
       <p className="mb-0">
@@ -11,9 +11,9 @@ export default function Person(props) {
       <p className="mb-0">Age: {dob.age}</p>
       <p>email: {email}</p>
 
-      <p>Category: {Math.floor(Math.random() * 3) + 1}</p>
+      {/* <p>Category: {Math.floor(Math.random() * 3) + 1}</p>
       <p>Job: {Math.floor(Math.random() * 3) + 1}</p>
-      <p>Location: {Math.floor(Math.random() * 6) + 1}</p>
+      <p>Location: {Math.floor(Math.random() * 6) + 1}</p> */}
     </div>
   );
 }
@@ -22,6 +22,4 @@ Person.propTypes = {
   name: PropTypes.object,
   dob: PropTypes.object,
   email: PropTypes.string,
-  onDelete: PropTypes.func,
-  id: PropTypes.object,
 };
