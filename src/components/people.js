9bf22fe17c7e50183aeeff5f700c4ba2ae2a008b
jbeltrami <joao_beltrami@hotmail.com>
2019-05-02 11:22:31 -0400

    Add gender filter to people

--- src/components/people.js
+++ src/components/people.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import Person from './person';
-import PeopleAgeFilter from './people-filter';
+import PeopleAgeFilter from './peopleAgeFilter';
+import PeopleGenderFilter from './peopleGenderFilter';
 
 class People extends Component {
   state = {
     people: [],
     ages: [20, 40, 60, 70],
+    genders: ['female', 'male'],
   };
 
   componentDidMount() {
@@ -21,6 +23,7 @@ class People extends Component {
         return apiResponse.sort((a, b) => a.name.last - b.name.last);
       })
       .then(res => {
+        console.log(res);
         this.setState(() => ({
           people: [...res],
         }));
@@ -30,6 +33,15 @@ class People extends Component {
   handleAgeFilter = age => {
     const { people } = this.state;
     const filteredPeople = people.filter(e => e.dob.age > age);
+
+    this.setState({
+      people: [...filteredPeople],
+    });
+  };
+
+  handleGenderFilter = gender => {
+    const { people } = this.state;
+    const filteredPeople = people.filter(e => e.gender === gender);
 
     this.setState({
       people: [...filteredPeople],
@@ -46,7 +58,7 @@ class People extends Component {
   };
 
   render() {
-    const { people, ages } = this.state;
+    const { people, ages, genders } = this.state;
 
     return (
       <div>
@@ -58,13 +70,21 @@ class People extends Component {
             </div>
           </div>
 
-          <div className="row">
+          <div className="row mt-4">
             <div className="col-lg-12">People Age Filter:</div>
           </div>
 
           <PeopleAgeFilter ages={ages} onFilter={this.handleAgeFilter} />
 
-          <div className="row align-item-center justify-content-center">
+          <div className="row mt-4">
+            <div className="col-lg-12">People Gender Filter:</div>
+          </div>
+          <PeopleGenderFilter
+            genders={genders}
+            onFilter={this.handleGenderFilter}
+          />
+
+          <div className="row align-item-center justify-content-center mt-4">
             {people.map((person, index) => (
               <Person {...person} key={index} onDelete={this.handleDelete} />
             ))}
--- src/components/person.js
+++ src/components/person.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Person(props) {
-  const { name, dob, email } = props;
+  const { name, dob, email, gender } = props;
   return (
     <div className="col-lg-3 my-4">
       <p className="mb-0">
         Name: {name.first} {name.last}
       </p>
       <p className="mb-0">Age: {dob.age}</p>
+      <p className="mb-0">Gender: {gender}</p>
       <p>email: {email}</p>
 
       {/* <p>Category: {Math.floor(Math.random() * 3) + 1}</p>
@@ -20,6 +21,7 @@ export default function Person(props) {
 
 Person.propTypes = {
   name: PropTypes.object,
+  gender: PropTypes.string,
   dob: PropTypes.object,
   email: PropTypes.string,
 };
